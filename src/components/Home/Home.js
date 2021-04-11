d76c8d29f34c29879bbf47d179bd4c597d359035
bilyvio <evalottaw69@gmail.com>
2021-04-11 13:13:46 +0300

    lab3

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+
+class Home extends React.Component {
+    render() {
+        return  (
+            <h1>Hello from home page</h1>
+        );
+    }
+}
+
+export default Home;
